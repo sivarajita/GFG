22cde06361ba94eb97eca161e2e68eca8d24a8df
sivarajita <rajitaalla@gmail.com>
2025-12-01 20:49:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool areAnagrams(string& s1, string& s2) {
+        // code here
+        if(s1.length() != s2.length()){
+            return false;
+        }
+        sort(s1.begin(),s1.end());
+        sort(s2.begin(),s2.end());
+        return s1==s2;
+    }
+};
