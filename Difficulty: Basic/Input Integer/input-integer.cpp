9f8560e497cbd24febc6dab139230fe44e22a5f7
sivarajita <rajitaalla@gmail.com>
2025-12-27 18:37:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Input Integer/input-integer.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Take input
+    int n;
+    cin >> n;
+    n+=10;
+    cout << n << endl;
+    // Add 10 and Print
+    return 0;
+}
