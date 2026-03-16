433f38d8b79022e71ce667067654b76b997cc01d
sivarajita <rajitaalla@gmail.com>
2026-03-16 20:17:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Palindrome String/palindrome-string.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    bool isPalindrome(string& s) {
+        // code here
+        int left=0,right=s.length()-1;
+        while(left<right){
+            while(left<right&&!alphaNum(s[left])){
+                left++;
+            }
+            while(right>left&&!alphaNum(s[right])){
+                right--;
+            }
+            if(tolower (s[left]) != tolower (s[right])){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+    bool alphaNum(char c){
+        return (c>='A'&&c<='Z'||
+                c>='a'&&c<='z'||
+                c>='0'&&c<='9'
+        );
+    }
+};
